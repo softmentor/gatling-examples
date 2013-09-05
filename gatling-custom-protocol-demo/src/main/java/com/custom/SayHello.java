42bdbe67a10051bf291611aed8513884138e7c6d
jiny.thattil <jiny.thattil@inmobi.com>
2013-09-05 22:48:07 +0530

    Minor change in variable assignment for the long values

---
+++ gatling-custom-protocol-demo/src/main/java/com/custom/SayHello.java
@@ -0,0 +1,22 @@
+/**
+ * Created with IntelliJ IDEA. User: jiny Date: 5/9/13 Time: 10:23 PM To change this template use
+ * File | Settings | File Templates.
+ */
+
+package com.custom;
+
+/**
+ * The type Say hello.
+ */
+public class SayHello {
+
+  /**
+   * Echo void.
+   *
+   * @param printString the print string
+   */
+  public final void echo(final String printString) {
+    System.out.println("Say:" + printString);
+  }
+
+}
---
+++ gatling-custom-protocol-demo/src/test/scala/custom/protocol/test/TestCustomProtocolSimulation.scala
@@ -0,0 +1,66 @@
+package custom.protocol.test
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jiny
+ * Date: 5/9/13
+ * Time: 9:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import io.gatling.core.Predef._
+import scala.concurrent.duration._
+import akka.actor.ActorRef;
+import io.gatling.core.action.builder.ActionBuilder
+import io.gatling.core.action.{Chainable, system}
+import bootstrap._
+import akka.actor.Props
+import io.gatling.core.result.message.{RequestMessage, KO, OK}
+import io.gatling.core.result.writer.DataWriter
+import com.custom._
+
+class TestCustomProtocolSimulation extends Simulation {
+
+  val mine = new ActionBuilder {
+    def build(next: ActorRef) = system.actorOf(Props(new MyAction(next)))
+  }
+  val scn = scenario("My custom protocol test")
+    .repeat(2) {
+                 exec(mine)
+               }
+
+  setUp(scn.inject(ramp(3 users) over (10 seconds)))
+}
+
+
+class MyAction(val next: ActorRef) extends Chainable {
+
+
+  def greet() {
+    val delegate = new SayHello
+    delegate.echo("Hi")
+  }
+
+  def execute(session: Session) {
+    var start: Long = 0L
+    var end: Long = 0L
+    var status: Status = OK
+    var errorMessage: Option[String] = None
+    try {
+      start = System.currentTimeMillis / 1000;
+      greet() // Call any custom code you wish, say an API call
+      end = System.currentTimeMillis / 1000;
+    } catch {
+      case e: Exception =>
+        errorMessage = Some(e.getMessage)
+        logger.error("FOO FAILED", e)
+        status = KO
+    } finally {
+      DataWriter.tell(
+        RequestMessage(session.scenarioName, session.userId, session.groupStack, "Test Scenario",
+                       start, start, end, end,
+                       status, errorMessage, Nil))
+      next ! session
+    }
+  }
+}
