5624dbb1e5daf326040a64b2add02482ad95f8e4
jiny.thattil <jiny.thattil@inmobi.com>
2014-12-20 15:56:24 +0530

    Upgrade to gatling 2.1.1, it is broken, but need to commit

--- gatling-custom-protocol-demo/src/test/scala/custom/protocol/test/TestCustomProtocolSimulation.scala
+++ gatling-custom-protocol-demo/src/test/scala/custom/protocol/test/TestCustomProtocolSimulation.scala
@@ -8,34 +8,55 @@ package custom.protocol.test
  * To change this template use File | Settings | File Templates.
  */
 
-import io.gatling.core.Predef._
-import scala.concurrent.duration._
-import akka.actor.ActorRef;
-import io.gatling.core.action.builder.ActionBuilder
-import io.gatling.core.action.{Chainable, system}
-import bootstrap._
-import assertions._
-import akka.actor.Props
-import io.gatling.core.result.message.{RequestMessage, KO, OK}
-import io.gatling.core.result.writer.DataWriter
+import akka.actor.{ActorRef, Props}
 import com.custom._
-import scalaj.collection.Imports._
+import io.gatling.core.Predef._
+import io.gatling.core.action.Chainable
+import io.gatling.core.action.builder.ActionBuilder
+import io.gatling.core.config.Protocols
+import io.gatling.core.result.message.{KO, OK}
+import io.gatling.core.result.writer.{DataWriter, RequestMessage}
+
+import scala.concurrent.duration._
+
+//import scalaj.collection.Imports._
+//import scala.collection.JavaConversions._
+
+import scala.collection.convert.wrapAsJava
 
 class TestCustomProtocolSimulation extends Simulation {
 
   val mine = new ActionBuilder {
-    def build(next: ActorRef) = system.actorOf(Props(new MyAction(next)))
+    def build(next: ActorRef, protocols: Protocols) = {
+      system.actorOf(Props(new MyAction(next)))
+    }
   }
-  val userLog = csv("user_credentials.csv").queue
+  val userLog = csv("user_credentials.csv").circular
   val scn = scenario("My custom protocol test")
     .feed(userLog)
     .repeat(2) {
-                 exec(mine)
-               }
+    exec(mine)
+  }
 
-  setUp(scn.inject(ramp(3 users) over (10 seconds)))
+  /*setUp(scn.inject(ramp(3 users) over (10 seconds)))
       //Assert the output max time is less than 50ms and 95% of requests were successful
-      .assertions(global.responseTime.max.lessThan(50),global.successfulRequests.percent.greaterThan(95))
+      .assertions(global.responseTime.max.lessThan(50),global.successfulRequests.percent.greaterThan(95))*/
+
+  setUp(
+    scn.inject(
+      nothingFor(4 seconds), // 1
+      atOnceUsers(10), // 2
+      rampUsers(3) over (5 seconds), // 3
+      constantUsersPerSec(5) during (5 seconds), // 4
+      constantUsersPerSec(5) during (5 seconds) randomized, // 5
+      rampUsersPerSec(10) to 20 during (1 minutes), // 6
+      rampUsersPerSec(10) to 20 during (1 minutes) randomized, // 7
+      splitUsers(10) into (rampUsers(2) over (10 seconds)) separatedBy (2 seconds), // 8
+      splitUsers(10) into (rampUsers(2) over (10 seconds)) separatedBy atOnceUsers(5), // 9
+      heavisideUsers(10) over (500 milliseconds) // 10
+    )
+    //Assert the output max time is less than 50ms and 95% of requests were successful
+  ).assertions(global.responseTime.max.lessThan(50), global.successfulRequests.percent.greaterThan(95))
 }
 
 
@@ -48,7 +69,7 @@ class MyAction(val next: ActorRef) extends Chainable {
 
     val delegate = new SayHello
     //using scalaj to convert from scala map to java map
-    delegate.echo("Hi", session.attributes.asJava)
+    delegate.echo("Hi", wrapAsJava.mapAsJavaMap(session.attributes))
   }
 
   def execute(session: Session) {
@@ -67,10 +88,23 @@ class MyAction(val next: ActorRef) extends Chainable {
         logger.error("FOO FAILED", e)
         status = KO
     } finally {
-      DataWriter.tell(
-        RequestMessage(session.scenarioName, session.userId, session.groupStack, "Test Scenario",
-                       start, start, end, end,
-                       status, errorMessage, Nil))
+      val requestStartDate, requestEndDate = start
+      val responseStartDate, responseEndDate = end
+      val requestName = "Test Scenario"
+      val message = errorMessage
+      val extraInfo = Nil
+      DataWriter.dispatch(RequestMessage(
+        session.scenarioName,
+        session.userId,
+        session.groupHierarchy,
+        requestName,
+        requestStartDate,
+        requestEndDate,
+        responseStartDate,
+        responseEndDate,
+        status,
+        message,
+        extraInfo))
       next ! session
     }
   }
